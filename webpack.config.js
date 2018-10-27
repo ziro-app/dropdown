f6a9bcc66a5c9d27eca9362a50d325b1a3633c87
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2018-10-26 17:35:01 -0700

    trim unnecessary configurations

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
         rules: [
             {
                 test: /\.js$/,
-                exclude: /(node_modules)/,
+                exclude: /node_modules/,
 				use: {
 					loader: 'babel-loader',
 					options: {
@@ -25,10 +25,6 @@ module.exports = {
 						]
 					}
 				}
-            },
-            {
-            	test: /\.css$/,
-            	use: [ 'style-loader', 'raw-loader' ]
             }
         ]
     }
