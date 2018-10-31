61e4290a3b7c3515599302d328070a229465b50d
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2018-10-30 20:24:50 -0700

    start designing dropdown component, add basic funcionality

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export const Field = () => ( <h1>Field</h1> )
+export default class Dropdown extends Component {
+	state = {
+		userInput: ''
+	}
+	handleChange = ({ target: { value }}) => this.setState({ userInput: value })
+	render = () => (
+		<div>
+			<input list='dropdown-options' onChange={this.handleChange} />
+			<datalist id='dropdown-options'>
+				{
+					this.props.options.map( (option, index) =>
+						<option key={index} value={option} />
+					)
+				}
+			</datalist>
+		</div>
+	)
+
+}
