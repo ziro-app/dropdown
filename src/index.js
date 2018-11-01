da36abe6ff7e01d5c0214de5b43270fe360f2cdd
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2018-10-31 20:12:04 -0700

    define new props

--- src/index.js
+++ src/index.js
@@ -5,10 +5,17 @@ export default class Dropdown extends Component {
 	state = {
 		userInput: ''
 	}
-	handleChange = ({ target: { value }}) => this.setState({ userInput: value })
+	handleChange = ({ target: { value }}) => {
+		this.setState({ userInput: value })
+		this.props.updateParent(this.props.name, value)
+	}
 	render = () => (
 		<div>
-			<input list='dropdown-options' onChange={this.handleChange} />
+			<input
+				list='dropdown-options'
+				placeholder={this.props.placeholder}
+				onChange={this.handleChange}
+			/>
 			<datalist id='dropdown-options'>
 				{
 					this.props.options.map( (option, index) =>
@@ -22,6 +29,8 @@ export default class Dropdown extends Component {
 }
 
 Dropdown.propTypes = {
+	name: PropTypes.string.isRequired, // must be the state name inside React parent component
+	placeholder: PropTypes.string,
 	options: PropTypes.arrayOf(PropTypes.string).isRequired,
 	updateParent: PropTypes.func.isRequired
 }
